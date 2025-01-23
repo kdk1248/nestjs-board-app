0964a18be5d108688933eedef815a1fec8acc333
kong <alice3976@g.hongik.ac.kr>
2025-01-23 15:41:42 +0900

    refactor: Repository 계층 의존성 주입 설정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { BoardsRepository } from './boards.repository';
 
 @Module({
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, BoardsRepository],
 })
 export class BoardsModule {}
