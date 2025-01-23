e1e16a30fdd27da5520dd5739edff554add342d9
kong <alice3976@g.hongik.ac.kr>
2025-01-23 15:49:19 +0900

    refactor: Controller 계층의 async/await 비동기 promise 반환으로 변경

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -14,47 +14,47 @@ export class BoardsController {
 
     // 게시글 조회 기능
     @Get('/')
-    getAllBoards(): Board[] {
-	    return this.boardsService.getAllBoards();
+    async getAllBoards(): Promise<Board[]> {
+        return await this.boardsService.getAllBoards(); // 비동기적으로 게시글 가져오기
     }
 
-    // 특정 게시글 조회 기능
-    @Get('/:id')
-    getBoardById( @Param('id') id: number): Board {
-        return this.boardsService.getBoardDetailById(id);
-    }
+//     // 특정 게시글 조회 기능
+//     @Get('/:id')
+//     getBoardById( @Param('id') id: number): Board {
+//         return this.boardsService.getBoardDetailById(id);
+//     }
 
-    //키워드(작성자)로 검색한 게시글 조회 기능
-    @Get('/search/:keyword')
-    getBoardsByKeyword(@Query('author') author: string): Board[] {
-        return this.boardsService.getBoardsByKeyword(author);
-    }
+//     //키워드(작성자)로 검색한 게시글 조회 기능
+//     @Get('/search/:keyword')
+//     getBoardsByKeyword(@Query('author') author: string): Board[] {
+//         return this.boardsService.getBoardsByKeyword(author);
+//     }
 
-    // 게시글 작성 기능
-    @Post('/')
-    createBoard(
-        @Body() createboardDto: CreateBoardDto) {
-        return this.boardsService.createBoard(createboardDto);
-    }
+//     // 게시글 작성 기능
+//     @Post('/')
+//     createBoard(
+//         @Body() createboardDto: CreateBoardDto) {
+//         return this.boardsService.createBoard(createboardDto);
+//     }
 
-    // 특정 번호의 게시글 수정
-    @Put('/:id')
-    updateBoardById(
-        @Param('id')id: number,
-        @Body()updateBoardDto : UpdateBoardDto): Board{
-        return this.boardsService.updateBoardById(id, updateBoardDto)
-    }
-    // 특정 번호의 게시글 일부 수정
-    @Patch('/:id')
-    updateBoardStatusById(
-        @Param('id')id: number, 
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
-        return this.boardsService.updateBoardStatusById(id, status);
-    }
+//     // 특정 번호의 게시글 수정
+//     @Put('/:id')
+//     updateBoardById(
+//         @Param('id')id: number,
+//         @Body()updateBoardDto : UpdateBoardDto): Board{
+//         return this.boardsService.updateBoardById(id, updateBoardDto)
+//     }
+//     // 특정 번호의 게시글 일부 수정
+//     @Patch('/:id')
+//     updateBoardStatusById(
+//         @Param('id')id: number, 
+//         @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
+//         return this.boardsService.updateBoardStatusById(id, status);
+//     }
 
-    // 게시글 삭제 기능
-    @Delete('/:id')
-    deleteBoardById(@Param('id')id: number): void {
-        this.boardsService.deleteBoardById(id);
-    }
+//     // 게시글 삭제 기능
+//     @Delete('/:id')
+//     deleteBoardById(@Param('id')id: number): void {
+//         this.boardsService.deleteBoardById(id);
+//     }
 }
