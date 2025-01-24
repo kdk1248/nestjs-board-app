11139d66ce653bfb9ed96614c4af1da605ebfad0
kong <alice3976@g.hongik.ac.kr>
2025-01-24 12:27:32 +0900

    chore: TypeORM의 엔터티 설정과, Service 계층의 Repository 주입

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,14 +1,17 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Board } from './boards.entity';
 import { BoardStatus } from './boards-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { UpdateBoardDto } from './dto/update-board.dto';
-import { BoardsRepository } from './boards.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable() 
 export class BoardsService {
     //Repository 계층 DI 
-    constructor(private boardsRepository: BoardsRepository){}
+    constructor(
+        @InjectRepository(Board)
+        private boardsRepository: Repository<Board>
+    ){}
 
     // 게시글 조회 기능  
     async getAllBoards(): Promise<Board[]> {  
