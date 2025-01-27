423598c2f55b8fe5ce8bb0adbfca91f78ab1abf7
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 20:51:41 +0900

    feat: 회원 가입 시, 비밀번호 암호화(BCrypt 해싱) 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { Repository } from 'typeorm';
 import { UserRole } from './users-role.enum';
+import * as bcrypt from 'bcryptjs'; 
 
 @Injectable()
 export class AuthService {
@@ -11,17 +12,20 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>
   ) { }
-  
+
   // 회원 가입 기능
   async createUser(createuserDto: CreateUserDto): Promise<User> {   //특수문자 x
-    const { email, username, password,role } = createuserDto;
-    if (!email || !username || !password !||role) {
+    const { email, username, password, role } = createuserDto;
+    if (!email || !username || !password! || role) {
       throw new BadRequestException('Something went wrong.') //특정 필드 언급하지않도록 -> 보안의 중요성성
     }
     await this.checkEmailExist(email);
 
+    // 비밀번호 해싱
+    const hashedPassword = await this.hashPassword(password);
+
     const newUser: User = {
-      id: 0, 
+      id: 0,
       email,
       username,
       password,
@@ -31,11 +35,15 @@ export class AuthService {
     return createUser;
   }
 
-  async checkEmailExist(email:string):Promise<void>{
-    const exisitngUser= await this.userRepository.findOne({where : {email}})
-    if(exisitngUser){
+  async checkEmailExist(email: string): Promise<void> {
+    const exisitngUser = await this.userRepository.findOne({ where: { email } })
+    if (exisitngUser) {
       throw new ConflictException('Email already exists');
     }
   }
 
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(); // 솔트 생성
+    return await bcrypt.hash(password, salt); // 비밀번호 해싱
+  }
 }
