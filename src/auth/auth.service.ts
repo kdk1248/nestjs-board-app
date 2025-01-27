cbfafaafe2defa4c4c90e891e6eb5de1b87c9952
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 22:20:12 +0900

    feat: [1] JWT 토큰 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException, Res, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
@@ -6,12 +6,15 @@ import { Repository } from 'typeorm';
 import { UserRole } from './users-role.enum';
 import * as bcrypt from 'bcryptjs';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>
+    private userRepository: Repository<User>,
+    private jwtService: JwtService
   ) { }
 
   // 회원 가입 기능
@@ -38,27 +41,36 @@ export class AuthService {
   }
 
   //로그인 기능
-  async signIn(loginUserDto: LoginUserDto): Promise<string> {
+  async signIn(loginUserDto: LoginUserDto, @Res() res: Response): Promise<string> {
     const { email, password } = loginUserDto;
-    
-    const existingUser = await this.checkEmailExist(email);
+    try {
+      const existingUser = await this.checkEmailExist(email);
 
-    if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
-      throw new UnauthorizedException('Invalid credentials');
+      if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      // [1] JWT 토큰 생성
+      const payload = {
+        id: existingUser.id,
+        email: existingUser.email,
+        username: existingUser.username,
+        role: existingUser.role
+      };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return accessToken;
+    } catch (error) {
+      throw error;
     }
-    const message = 'Login success';
-    return message;
-
   }
-
-  async findUserByEmail(email: string): Promise<User>{
-    const exisitngUser = await this.userRepository.findOne({where: {email}});
-    if(!exisitngUser){
+  async findUserByEmail(email: string): Promise<User> {
+    const exisitngUser = await this.userRepository.findOne({ where: { email } });
+    if (!exisitngUser) {
       throw new NotFoundException('User not found');
     }
     return exisitngUser;
   }
-  
+
   async checkEmailExist(email: string): Promise<User> {
     const exisitngUser = await this.userRepository.findOne({ where: { email } })
     if (!exisitngUser) {
