1c3eb8ac50627173a5fb85bc8c659be360979125
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 21:54:45 +0900

    chore: JWT 모듈 및 연결 의존성 설정정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/users.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import * as dotenv from 'dotenv';
+import { User } from './entities/users.entity';
+
+dotenv.config();
+
 @Module({
-  imports:[
-    TypeOrmModule.forFeature([User]), //Board 엔터티를 TypeORM 모듈에 등록
-
-
-
+  imports: [
+	  PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions:{
+        expiresIn: process.env.JWT_EXPIRATION,
+      }
+    }),
+    TypeOrmModule.forFeature([User])
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService]
 })
-export class UserModule {}
+export class AuthModule {}
