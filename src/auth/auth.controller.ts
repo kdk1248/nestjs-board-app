8f9ad61d9d47a23309879cb02d13b60b14fb2ad3
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 22:20:39 +0900

    feat: [2] JWT 를 쿠키에 저장

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserResponseDto } from './dto/user-response.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { Response } from 'express'; 
+import { log } from 'console';
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   // 회원 가입 기능
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-    const userResponseDto = new UserResponseDto(await this.authService.createUser(createUserDto))
+    const userResponseDto = new UserResponseDto(
+      await this.authService.createUser(createUserDto),
+    );
     return userResponseDto;
   }
 
   // 로그인 기능
   @Post('/signin')
-  async signin(@Body() loginUserDto: LoginUserDto): Promise<string>{
-    const message = this.authService.signIn(loginUserDto);
-    return message;
-
+  async signin(@Body() loginUserDto: LoginUserDto, @Res() res: Response): Promise<void> {
+    const accessToken = await this.authService.signIn(loginUserDto,res);
+    
+    // [2] JWT를 쿠키에 저장
+    res.cookie('Authorization', accessToken, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 360000,
+      sameSite: 'none'
+    });
+    res.send({ message: "Login Success" });
   }
 }
