db82db8ad8f8ec6d5332e548aa0c9862e9a17bd2
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 14:10:34 +0900

    refactor: ValidstionPipe 전역 설정으로 변경경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { BoardsModule } from './boards/boards.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { GlobalModule } from './global.module';
 @Module({
   imports: [
+    GlobalModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     BoardsModule
   ],
