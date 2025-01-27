2a7f1b19b242879f11b495521b73fcc33e1a980b
kdk1248 <alice3976@g.hongik.ac.kr>
2025-01-27 14:46:03 +0900

    refactor: 회원가입 중 이메일 중복에 대한 처리리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
@@ -11,12 +11,15 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>
   ) { }
-  // 게시글 작성 기능
+  
+  // 회원 가입 기능
   async createUser(createuserDto: CreateUserDto): Promise<User> {   //특수문자 x
     const { email, username, password,role } = createuserDto;
     if (!email || !username || !password !||role) {
       throw new BadRequestException('Something went wrong.') //특정 필드 언급하지않도록 -> 보안의 중요성성
     }
+    await this.checkEmailExist(email);
+
     const newUser: User = {
       id: 0, 
       email,
@@ -26,6 +29,13 @@ export class AuthService {
     };
     const createUser = await this.userRepository.save(newUser);
     return createUser;
+  }
+
+  async checkEmailExist(email:string):Promise<void>{
+    const exisitngUser= await this.userRepository.findOne({where : {email}})
+    if(exisitngUser){
+      throw new ConflictException('Email already exists');
+    }
   }
 
 }
