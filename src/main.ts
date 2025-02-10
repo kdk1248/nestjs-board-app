e41595f4c04ae57cbf3b3d3424601e510812397e
kdk1248 <alice3976@g.hongik.ac.kr>
2025-02-10 15:36:58 +0900

    temp: 프론트엔드 구현을 위한 임시 주석 및 CORS 적용

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin:'http://loalhost:4200',
+    credentials:true,
+    exposedHeaders: [ 'Authorization']
+  })
   await app.listen(process.env.PORT);
   Logger.log(`Application Running on Port: ${process.env.PORT}`)
 }
